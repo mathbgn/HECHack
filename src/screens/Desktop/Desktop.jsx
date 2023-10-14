8c7aeffc585503c0b7c97d61cd039d6c3fc17ce7
MathBoug <44569769+mathbgn@users.noreply.github.com>
2023-10-14 18:40:53 +0200

    Add files via upload

---
+++ src/screens/Desktop/Desktop.jsx
@@ -0,0 +1,376 @@
+import React from "react";
+import "./style.css";
+
+export const Desktop = () => {
+  return (
+    <div className="desktop">
+      <div className="div">
+        <div className="overlap">
+          <img className="screenshot" alt="Screenshot" src="/img/screenshot-2023-10-01-at-22-05-1.png" />
+          <div className="frame">
+            <div className="auto-layout">
+              <div className="group" />
+              <div className="frame-2">
+                <div className="text-wrapper">Documentation</div>
+                <div className="text-wrapper-2">API reference</div>
+                <div className="text-wrapper-3">Examples</div>
+              </div>
+            </div>
+            <div className="frame-3">
+              <div className="text-wrapper-4">Examples</div>
+              <div className="div-wrapper">
+                <div className="text-wrapper-5">Connect Wallet</div>
+              </div>
+            </div>
+          </div>
+          <div className="group-2">
+            <div className="text-wrapper-6">Learn. Build. Grow.</div>
+            <p className="get-acknowledged-get">
+              Get Acknowledged, Get Rewarded, and Improve <br />
+              Your Development Capabilities
+            </p>
+            <div className="frame-4">
+              <div className="text-wrapper-7">Supported by</div>
+              <img className="tezos-logo" alt="Tezos logo" src="/img/tezos-logo.svg" />
+            </div>
+          </div>
+        </div>
+        <div className="frame-5">
+          <div className="text-wrapper-8">Upcoming</div>
+          <div className="text-wrapper-9">Active</div>
+          <div className="span-statusfilters">
+            <div className="text-wrapper-10">1</div>
+          </div>
+          <div className="span-statusfilters-2">
+            <div className="text-wrapper-10">1</div>
+          </div>
+          <div className="div-statusfilters" />
+          <div className="div-statusfilters-2">
+            <div className="text-wrapper-11">All rounds</div>
+            <div className="span-statusfilters-3">
+              <div className="text-wrapper-12">2</div>
+            </div>
+          </div>
+          <div className="overlap-group">
+            <div className="input">
+              <input className="div-placeholder" placeholder="Search" type="text" />
+            </div>
+            <img className="SVG" alt="Svg" src="/img/svg.svg" />
+          </div>
+        </div>
+        <div className="div-card-card">
+          <div className="div-roundcard">
+            <div className="overlap-group-2">
+              <div className="div-roundcard-2">
+                <div className="div-roundcard-3">
+                  <p className="p">WEB 3.0 Hackathon #2 @ HEC</p>
+                </div>
+                <div className="span-statuspill">
+                  <div className="text-wrapper-13">Active</div>
+                </div>
+              </div>
+              <div className="div-roundcard-4">
+                <div className="text-wrapper-14">Sponsored by Tezos Foundation</div>
+              </div>
+            </div>
+            <div className="span-roundcard">
+              <p className="bringing-together">
+                Bringing together professionals and students to bring to life new ideas and projects
+                about&nbsp;&nbsp;blockchain, WEB 3, cryptocurrency, NFT, DeFi, security or other uses...
+              </p>
+            </div>
+          </div>
+          <div className="div-roundcard-5">
+            <div className="div-2">
+              <div className="p-roundcard-title">
+                <div className="text-wrapper-15">Awards</div>
+              </div>
+              <div className="p-roundcard-info">
+                <div className="text-wrapper-16">+80,000 XTZ</div>
+              </div>
+            </div>
+            <div className="div-roundcard-6" />
+            <div className="div-3">
+              <div className="p-roundcard-title-2">
+                <div className="text-wrapper-17">Deadline</div>
+              </div>
+              <div className="p-roundcard-info-2">
+                <div className="text-wrapper-18">in 1 day</div>
+              </div>
+            </div>
+            <div className="div-roundcard-7" />
+            <div className="overlap-group-wrapper">
+              <div className="overlap-group-3">
+                <div className="p-roundcard-title-3">
+                  <div className="text-wrapper-19">Projects</div>
+                </div>
+                <div className="text-wrapper-20">15</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="div-card-card-eer">
+          <div className="div-roundcard">
+            <div className="overlap-group-2">
+              <div className="div-roundcard-2">
+                <div className="div-roundcard-3">
+                  <p className="p">WEB 3.0 Hackathon #2 @ HEC</p>
+                </div>
+                <div className="span-statuspill">
+                  <div className="text-wrapper-13">Active</div>
+                </div>
+              </div>
+              <div className="div-roundcard-4">
+                <div className="text-wrapper-14">Sponsored by Tezos Foundation</div>
+              </div>
+            </div>
+            <div className="span-roundcard">
+              <p className="bringing-together">
+                Bringing together professionals and students to bring to life new ideas and projects
+                about&nbsp;&nbsp;blockchain, WEB 3, cryptocurrency, NFT, DeFi, security or other uses...
+              </p>
+            </div>
+          </div>
+          <div className="div-roundcard-5">
+            <div className="div-2">
+              <div className="p-roundcard-title">
+                <div className="text-wrapper-15">Awards</div>
+              </div>
+              <div className="p-roundcard-info">
+                <div className="text-wrapper-16">+80,000 XTZ</div>
+              </div>
+            </div>
+            <div className="div-roundcard-6" />
+            <div className="div-3">
+              <div className="p-roundcard-title-2">
+                <div className="text-wrapper-17">Deadline</div>
+              </div>
+              <div className="p-roundcard-info-2">
+                <div className="text-wrapper-18">in 1 day</div>
+              </div>
+            </div>
+            <div className="div-roundcard-7" />
+            <div className="overlap-group-wrapper">
+              <div className="overlap-group-3">
+                <div className="p-roundcard-title-3">
+                  <div className="text-wrapper-19">Projects</div>
+                </div>
+                <div className="text-wrapper-20">15</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="div-card-card-2">
+          <div className="div-roundcard">
+            <div className="overlap-group-2">
+              <div className="div-roundcard-2">
+                <div className="div-roundcard-3">
+                  <p className="p">WEB 3.0 Hackathon #2 @ HEC</p>
+                </div>
+                <div className="span-statuspill">
+                  <div className="text-wrapper-13">Active</div>
+                </div>
+              </div>
+              <div className="div-roundcard-4">
+                <div className="text-wrapper-14">Sponsored by Tezos Foundation</div>
+              </div>
+            </div>
+            <div className="span-roundcard">
+              <p className="bringing-together">
+                Bringing together professionals and students to bring to life new ideas and projects
+                about&nbsp;&nbsp;blockchain, WEB 3, cryptocurrency, NFT, DeFi, security or other uses...
+              </p>
+            </div>
+          </div>
+          <div className="div-roundcard-5">
+            <div className="div-2">
+              <div className="p-roundcard-title">
+                <div className="text-wrapper-15">Awards</div>
+              </div>
+              <div className="p-roundcard-info">
+                <div className="text-wrapper-16">+80,000 XTZ</div>
+              </div>
+            </div>
+            <div className="div-roundcard-6" />
+            <div className="div-3">
+              <div className="p-roundcard-title-2">
+                <div className="text-wrapper-17">Deadline</div>
+              </div>
+              <div className="p-roundcard-info-2">
+                <div className="text-wrapper-18">in 1 day</div>
+              </div>
+            </div>
+            <div className="div-roundcard-7" />
+            <div className="overlap-group-wrapper">
+              <div className="overlap-group-3">
+                <div className="p-roundcard-title-3">
+                  <div className="text-wrapper-19">Projects</div>
+                </div>
+                <div className="text-wrapper-20">15</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="div-card-card-3">
+          <div className="div-roundcard">
+            <div className="div-roundcard-8">
+              <div className="div-roundcard-9">
+                <div className="text-wrapper-21">Garden Round 1</div>
+              </div>
+              <div className="span-statuspill-2">
+                <div className="text-wrapper-22">Upcoming</div>
+              </div>
+            </div>
+            <div className="an-open-round-where-wrapper">
+              <p className="an-open-round-where">
+                An open round where builders are encouraged to propose
+                <br />
+                any idea they believe will benefit the Nouns ecosystem.
+                <br />
+                Anyone with an ETH address is welcome to propose. Once…
+              </p>
+            </div>
+          </div>
+          <div className="div-roundcard-5">
+            <div className="div-2">
+              <div className="p-roundcard-title">
+                <div className="text-wrapper-15">Awards</div>
+              </div>
+              <div className="element-ETH-wrapper">
+                <p className="element-ETH">
+                  <span className="span">5 ETH</span>
+                  <span className="text-wrapper-23"> × </span>
+                  <span className="span">2</span>
+                </p>
+              </div>
+            </div>
+            <div className="div-roundcard-6" />
+            <div className="div-3">
+              <div className="p-roundcard-title-4">
+                <div className="text-wrapper-24">Round starts</div>
+              </div>
+              <div className="p-roundcard-info-3">
+                <div className="text-wrapper-18">in 9 days</div>
+              </div>
+            </div>
+            <div className="div-roundcard-7" />
+            <div className="overlap-group-wrapper">
+              <div className="overlap-group-3">
+                <div className="p-roundcard-title-3">
+                  <div className="text-wrapper-19">Proposals</div>
+                </div>
+                <div className="text-wrapper-25">0</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="div-card-card-4">
+          <div className="div-roundcard">
+            <div className="div-roundcard-8">
+              <div className="div-roundcard-9">
+                <div className="text-wrapper-21">Garden Round 1</div>
+              </div>
+              <div className="span-statuspill-2">
+                <div className="text-wrapper-22">Upcoming</div>
+              </div>
+            </div>
+            <div className="an-open-round-where-wrapper">
+              <p className="an-open-round-where">
+                An open round where builders are encouraged to propose
+                <br />
+                any idea they believe will benefit the Nouns ecosystem.
+                <br />
+                Anyone with an ETH address is welcome to propose. Once…
+              </p>
+            </div>
+          </div>
+          <div className="div-roundcard-5">
+            <div className="div-2">
+              <div className="p-roundcard-title">
+                <div className="text-wrapper-15">Awards</div>
+              </div>
+              <div className="element-ETH-wrapper">
+                <p className="element-ETH">
+                  <span className="span">5 ETH</span>
+                  <span className="text-wrapper-23"> × </span>
+                  <span className="span">2</span>
+                </p>
+              </div>
+            </div>
+            <div className="div-roundcard-6" />
+            <div className="div-3">
+              <div className="p-roundcard-title-4">
+                <div className="text-wrapper-24">Round starts</div>
+              </div>
+              <div className="p-roundcard-info-3">
+                <div className="text-wrapper-18">in 9 days</div>
+              </div>
+            </div>
+            <div className="div-roundcard-7" />
+            <div className="overlap-group-wrapper">
+              <div className="overlap-group-3">
+                <div className="p-roundcard-title-3">
+                  <div className="text-wrapper-19">Proposals</div>
+                </div>
+                <div className="text-wrapper-25">0</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="div-card-card-5">
+          <div className="div-roundcard">
+            <div className="div-roundcard-8">
+              <div className="div-roundcard-9">
+                <div className="text-wrapper-21">Garden Round 1</div>
+              </div>
+              <div className="span-statuspill-2">
+                <div className="text-wrapper-22">Upcoming</div>
+              </div>
+            </div>
+            <div className="an-open-round-where-wrapper">
+              <p className="an-open-round-where">
+                An open round where builders are encouraged to propose
+                <br />
+                any idea they believe will benefit the Nouns ecosystem.
+                <br />
+                Anyone with an ETH address is welcome to propose. Once…
+              </p>
+            </div>
+          </div>
+          <div className="div-roundcard-5">
+            <div className="div-2">
+              <div className="p-roundcard-title">
+                <div className="text-wrapper-15">Awards</div>
+              </div>
+              <div className="element-ETH-wrapper">
+                <p className="element-ETH">
+                  <span className="span">5 ETH</span>
+                  <span className="text-wrapper-23"> × </span>
+                  <span className="span">2</span>
+                </p>
+              </div>
+            </div>
+            <div className="div-roundcard-6" />
+            <div className="div-3">
+              <div className="p-roundcard-title-4">
+                <div className="text-wrapper-24">Round starts</div>
+              </div>
+              <div className="p-roundcard-info-3">
+                <div className="text-wrapper-18">in 9 days</div>
+              </div>
+            </div>
+            <div className="div-roundcard-7" />
+            <div className="overlap-group-wrapper">
+              <div className="overlap-group-3">
+                <div className="p-roundcard-title-3">
+                  <div className="text-wrapper-19">Proposals</div>
+                </div>
+                <div className="text-wrapper-25">0</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
